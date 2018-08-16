899e806f7171688b0802d91b3f0a16d7a8b44a0b
Ramsey Ramos <ramsey.ramos@gmail.com>
2018-08-17 00:18:03 +0800

    updated echo examples.

--- examples/echo/main.go
+++ examples/echo/main.go
@@ -3,28 +3,17 @@ package main
 import (
 	"net/http"
 
-	validator "github.com/kewlburn/go-validator"
 	"github.com/labstack/echo"
 )
 
-type (
-	User struct {
-		Name  string `json:"name" valid:"required"`
-		Email string `json:"email" valid:"required,email"`
-	}
-
-	CustomValidator struct {
-		validator *validator.Validator
-	}
-)
-
-func (cv *CustomValidator) Validate(i interface{}) error {
-	return cv.validator.ValidateStruct(i)
+type User struct {
+	Name  string `json:"name" valid:"required"`
+	Email string `json:"email" valid:"required,email"`
 }
 
 func main() {
 	e := echo.New()
-	e.Validator = &CustomValidator{validator: validator.New()}
+	e.Validator = &DefaultValidator{}
 	e.POST("/users", func(c echo.Context) (err error) {
 		u := new(User)
 		if err = c.Bind(u); err != nil {
@@ -35,5 +24,5 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, u)
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
