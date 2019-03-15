180a3d916fb26a7043ce8b7c01126f3ba20323ed
s.y.s <s.y.s.sam.sys@gmail.com>
2019-03-15 12:22:34 +0800

    update custom rule example

--- _examples/custom/main.go
+++ _examples/custom/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"regexp"
 
 	validator "github.com/syssam/go-validator"
 )
 
 // User contains user information
 type User struct {
-	FirstName string `valid:"customValidator"`
-	LastName  string `valid:"customValidator2"`
+	UserName string `valid:"customValidator"`
+	Password string `valid:"customValidator2"`
 }
 
 func CustomValidator(v reflect.Value, o reflect.Value, validTag *validator.ValidTag) bool {
@@ -19,15 +20,19 @@ func CustomValidator(v reflect.Value, o reflect.Value, validTag *validator.Valid
 
 func main() {
 	validator.MessageMap["customValidator"] = "customValidator is not valid."
-	validator.MessageMap["customValidator2"] = "customValidator2 is not valid."
+	validator.MessageMap["customValidator2"] = "Beginning with a letter, allowing 5-16 bytes, allowing alphanumeric underlining."
 	validator.CustomTypeRuleMap.Set("customValidator", CustomValidator)
 	validator.CustomTypeRuleMap.Set("customValidator2", func(v reflect.Value, o reflect.Value, validTag *validator.ValidTag) bool {
+		switch v.Kind() {
+		case reflect.String:
+			return regexp.MustCompile("^[a-zA-Z]\\w{5,17}$").MatchString(v.String())
+		}
 		return false
 	})
 
 	user := &User{
-		FirstName: "Badger",
-		LastName:  "Smith",
+		UserName: "Tester",
+		Password: "12345678",
 	}
 
 	err := validator.ValidateStruct(user)
