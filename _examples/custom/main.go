3c700857dcba3b4e8f8d384bd886959efd3e2c46
s.y.s <s.y.s.sam.sys@gmail.com>
2019-03-15 11:30:00 +0800

    update custom rule example

--- _examples/custom/main.go
+++ _examples/custom/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 
 	validator "github.com/syssam/go-validator"
 )
+
+// User contains user information
+type User struct {
+	FirstName string `valid:"customValidator"`
+	LastName  string `valid:"customValidator2"`
+}
 
 func CustomValidator(v reflect.Value, o reflect.Value, validTag *validator.ValidTag) bool {
 	return false
 }
 
 func main() {
+	validator.MessageMap["customValidator"] = "customValidator is not valid."
+	validator.MessageMap["customValidator2"] = "customValidator2 is not valid."
 	validator.CustomTypeRuleMap.Set("customValidator", CustomValidator)
 	validator.CustomTypeRuleMap.Set("customValidator2", func(v reflect.Value, o reflect.Value, validTag *validator.ValidTag) bool {
 		return false
 	})
+
+	user := &User{
+		FirstName: "Badger",
+		LastName:  "Smith",
+	}
+
+	err := validator.ValidateStruct(user)
+	if err != nil {
+		for _, err := range err.(validator.Errors) {
+			fmt.Println(err)
+		}
+		return
+	}
 }
